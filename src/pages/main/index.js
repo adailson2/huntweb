31472e6ba33820df8005193883c3013e93ab8794
Adailson Santos <adailson2@gmail.com>
2020-02-25 13:23:36 -0300

    show products with states logical

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,17 +3,21 @@ import api from '../../services/api';
 
 export default class  Main extends Component {
   
+  state = {
+    products: []
+  }
+
   componentDidMount(){
     this.loadProducts(); 
   }
 
   loadProducts = async () => {
     const response = await api.get(`/products`);
-
-    console.log(response.data);
+    
+    this.setState({ products: response.data.docs });
   };
 
   render(){
-    return <h1>Hello World. </h1>
+  return <h1>Contagem de produtos: {this.state.products.length}</h1>
   }
 }
