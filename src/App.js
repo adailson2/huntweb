b05913e204a606e95a5f187ea1ed3ef0a35b01fc
Adailson Santos <adailson2@gmail.com>
2020-02-25 12:57:19 -0300

    submit initial project and header

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import './styles.css'
+const App = () => (
+  <div className="App">
+    <Header></Header>
+  </div>
+);
+
+export default App;
